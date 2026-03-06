cb50f5d86ad92f8d7c8c97f5215a106a8b84fdbf
CrissyDev <christalriziki@gmail.com>
2026-03-07 01:02:29 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1 style={{ textAlign: "center", marginTop: "40px" }}>
+        Welcome to Sam Portfolio
+      </h1>
+    </div>
+  );
+}
+
+export default App;
