f5167419bb5e740c637322caca52825a6d3f8bf4
CrissyDev <christalriziki@gmail.com>
2026-03-10 13:05:29 +0300

    contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Contact Me</h1>
+      <p>This is the placeholder for the Contact page.</p>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Navbar.css"; 
 
 const Navbar = () => {
@@ -7,9 +8,9 @@ const Navbar = () => {
       <div className="logo">Sam's Portfolio</div>
 
       <ul className="nav-links">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
