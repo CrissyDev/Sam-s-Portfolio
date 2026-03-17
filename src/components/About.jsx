cd788c60d9aba66a90d08d69c3aaec5680022f7e
Mwanaz <saidsuleimanamarah@gmail>
2026-03-17 10:47:47 +0300

    about page editted

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/About.css"; 
+
+const About = () => {
+  return (
+
+    <div>
+      <h1 className="heading" style={{ textAlign: "center", marginTop: "40px" }}>About me</h1>
+      <img src="public/Sam-s-Portfolio/public/port image.png" alt="About me"/>
+      <p className="paragraph ">I am a UI/UX designer and web developer currently pursuing a BScIT at KCA University. My background is a blend of technical precision and business leadership—ranging from managing finances for SMEs to building digital brand identities.
+​I believe that technology should feel human. Whether I’m coding an interactive website or simplifying complex financial concepts for a client, my goal is to turn ambitious ideas into joyful, user-centered experiences. When I’m not in VS Code, you’ll find me sewing, writing poetry, or overthinking a new recipe while watching cooking shows.</p>
+    </div>
+     
+
+    
+  );
+};
